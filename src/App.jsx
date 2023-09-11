fe643c9b52dd8f4cb6c561adffd39126445e1aff
b1ink0 <loginfor357@gmail.com>
2023-09-11 15:25:28 +0530

    comments-added-for-components-and-reduced-code

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import Home from "./components/Home";
 import { useStateContext } from "./context/StateContext";
 import { useEffect } from "react";
 
+// Initialize Database
 const initializeDb = new Dexie("TreeNotes");
+// Create Database Schema
 initializeDb.version(1).stores({
   treeNotes: "++id, refId, title, root, createdAt, updatedAt",
 });
 
 const App = () => {
   const { setDb } = useStateContext();
+  // Initialize Database on App Load
   useEffect(() => {
     const init = async () => {
+      // Open Database
       const initDb = await initializeDb.open();
+      // Set Database to Context
       setDb(initDb);
     };
     init();
