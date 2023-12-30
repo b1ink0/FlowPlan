98c10745a37adfe7dfb13946d30140d62aaeaedc
b1ink0 <loginfor357@gmail.com>
2023-12-30 21:24:30 +0530

    add: task, number list option with customization added to doc view, added delete single item button

--- src/assets/Icons/DiamondDot.jsx
+++ src/assets/Icons/DiamondDot.jsx
@@ -4,12 +4,12 @@ function DiamondDot() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-full w-full group-hover:scale-110 transition-transform duration-300 shrink-0"
+      className="h-full w-full group-hover:scale-110 transition-transform duration-300 shrink-0 fill-current"
       viewBox="0 0 512 512"
     >
       <path
         d="M256 0L59.83 256 256 512l196.17-256z"
-        className="fill-[var(--logo-primary)]"
+        className="fill-current"
       ></path>
     </svg>
   );
---
+++ src/assets/Icons/RomanLIstIcon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function RomanListIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-full w-full group-hover:scale-110 transition-transform duration-300 shrink-0 fill-current"
+      viewBox="0 0 512 512"
+    >
+      <path
+        d="M58.817 90H512v30H58.817zm0-60H512v30H58.817zm61.808 257.075H512v30H120.625zm0-60H512v30H120.625zM178.742 482H512v30H178.742zm0-60H512v30H178.742zM0 0h30v120H0zm0 197.075h30v120H0zm59.464 0h30v120h-30zM0 392h30v120H0zm59.464 0h30v120h-30zm59.465 0h30v120h-30z"
+        className="fill-current"
+      ></path>
+    </svg>
+  );
+}
+
+export default React.memo(RomanListIcon);
