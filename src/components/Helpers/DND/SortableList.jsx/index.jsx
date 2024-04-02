4e603c77c6019ad75ceabe4ef003e47077e3f106
b1ink0 <76063440+b1ink0@users.noreply.github.com>
2024-04-02 23:10:28 +0530

    add: dnd for doc view fields

---
+++ src/components/Helpers/DND/SortableList.jsx/index.jsx
@@ -0,0 +1,68 @@
+// @ts-check
+import React, { useMemo, useState } from "react";
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable";
+
+// import "./SortableList.css";
+
+import { DragHandle, SortableItem, } from "../Sortable";
+import { SortableOverlay } from "../Overlay"
+
+export function SortableList({ items, onChange, renderItem }) {
+  const [active, setActive] = useState(null);
+  const activeItem = useMemo(
+    () => items.find((item) => item.id === active?.id),
+    [active, items]
+  );
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  return (
+    <DndContext
+      sensors={sensors}
+      onDragStart={({ active }) => {
+        setActive(active);
+      }}
+      onDragEnd={({ active, over }) => {
+        if (over && active.id !== over?.id) {
+          const activeIndex = items.findIndex(({ id }) => id === active.id);
+          const overIndex = items.findIndex(({ id }) => id === over.id);
+
+          onChange(arrayMove(items, activeIndex, overIndex));
+        }
+        setActive(null);
+      }}
+      onDragCancel={() => {
+        setActive(null);
+      }}
+    >
+      <SortableContext items={items}>
+        <ul className="SortableList list-none w-full" role="application">
+          {items.map((item) => (
+            <React.Fragment key={item.id}>{renderItem(item, active)}</React.Fragment>
+          ))}
+        </ul>
+      </SortableContext>
+      <SortableOverlay>
+        {activeItem ? renderItem(activeItem, active) : null}
+      </SortableOverlay>
+    </DndContext>
+  );
+}
+
+SortableList.Item = SortableItem;
+SortableList.DragHandle = DragHandle;
