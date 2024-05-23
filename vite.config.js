81717289467490a157c18e6c914ce059e8567ed0
b1ink0 <76063440+b1ink0@users.noreply.github.com>
2024-05-23 19:27:04 +0530

    fix: vercel deployment 1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import ViteRsw from 'vite-plugin-rsw'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import ViteRsw from "vite-plugin-rsw";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteRsw(),
-  ],
-})
+  plugins: [react(), ViteRsw()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
